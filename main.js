291466ac59ef695aaf9e78e9a52c7b7f09784776
Ikerstreamer <ikerdeaguirre@hotmail.com>
2018-02-08 18:27:48 -0800

    Try this

--- main.js
+++ main.js
@@ -5,19 +5,21 @@ var player = {
     unlock: false,
     spectrum: 0,
     specreset: 0,
-    spectrumLevel: [1,1,1,1,0,0],
+    spectrumLevel: [0,0,0,0,0,0],
     specced: 0,
 }
 
+var Cores = 1;
+var Clock = 1;
 var RUM = 1;
-var SML = [50, 100, 50, 50, 1, 1];
+var SML = [5, 10, 50, 50, 1, 1];
 var tab = "RGB";
 var price = { red: 5, green: 5, blue: [0, 0, 0, 0] };
 var income = {red:0, green:0, blue: 0};
 var click = 5;
 var auto = 0;
-var RSM = 0;
 var IG = 0;
+var IR = 0;
 var RSS = 0;
 var PD = 0;
 var SG = 0;
@@ -31,8 +33,8 @@ function bar(n,r,g,b,elemid) {
     this.mouse = 0;
     this.draw = function () {
         if (this.mouse == 1) increase(click);
-        if (income[this.name] >= 10) this.element.style.width = "100%";
-        else this.element.style.width = this.width + "%";
+        if (income[this.name] >= 1) this.element.style.width = "100%";
+        else this.element.style.width = this.width/2.56 + "%";
         this.element.style.background = RGBstring(this.color);
     }
     this.setup = function () {
@@ -60,8 +62,25 @@ function init() {
         player.version = v;
     }
     for (var i = 0; i < Object.keys(player.bars).length ; i++) player.bars[Object.keys(player.bars)[i]].draw();
-    setInterval(gameLoop, 20);
     setInterval(save, 3000);
+
+    window.start = Date.now();
+    window.clock = 0;
+    window.frameTime = 20;
+
+    setInterval(function () {
+        var now = Date.now();
+        clock += frameTime;
+        var dif = now - start - clock;
+
+        gameLoop();
+
+        while (dif >= frameTime) {
+            gameLoop();
+            clock += frameTime;
+            dif -= frameTime;
+        }
+    }, 20)
 }
 
 function gameLoop() {
@@ -78,20 +97,20 @@ function gameLoop() {
     for (var i = 0; i < Object.keys(player.money).length; i++) {
         var tempKey = Object.keys(player.money)[i];
         document.getElementById(tempKey + "Count").innerHTML = formatNum(player.money[tempKey]);
-        if (income[tempKey] >= 10) document.getElementById(tempKey + "Bar").innerHTML = formatNum(displayIncome(income[tempKey])) + "/s";
+        if (income[tempKey] >= 1) document.getElementById(tempKey + "Bar").innerHTML = formatNum(displayIncome(income[tempKey])) + "/s";
         else document.getElementById(tempKey + "Bar").innerHTML = "";
         if (tempKey == "blue") {
             for (var j = 0; j < 4; j++) {
                 document.getElementById(tempKey + "Button" + j).childNodes[1].innerHTML = "Level: " + formatNum(player.level[tempKey][j], 0);
                 document.getElementById(tempKey + "Button" + j).childNodes[2].innerHTML = "Price: " + formatNum(price[tempKey][j]) + " " + tempKey;
                 switch (j) {
-                    case 0: document.getElementById(tempKey + "Button" + j).childNodes[3].innerHTML = "Current multi: " + formatNum(RSM) + "x";
+                    case 0: document.getElementById(tempKey + "Button" + j).childNodes[3].innerHTML = "Current speed: " + formatNum(Clock, 0, "Hz");
                         break
-                    case 1: document.getElementById(tempKey + "Button" + j).childNodes[3].innerHTML = "Current multi: " + formatNum(PD) + "x";
+                    case 1: document.getElementById(tempKey + "Button" + j).childNodes[3].innerHTML = "Current fill: " + (IR / 256 >= 1 ? formatNum(Math.floor(IR / 256), 0) + " & " : "") + formatNum(IR % 256,0) + "/256";
                         break
-                    case 2: document.getElementById(tempKey + "Button" + j).childNodes[3].innerHTML = "Current increase: " + formatNum(IG) + "%";
+                    case 2: document.getElementById(tempKey + "Button" + j).childNodes[3].innerHTML = "Current fill: " + (IG / 256 >= 1 ? formatNum(Math.floor(IG / 256), 0) + " & " : "") + formatNum(IG % 256, 0) + "/256";
                         break
-                    case 3: document.getElementById(tempKey + "Button" + j).childNodes[3].innerHTML = "Current gain: " + formatNum(SG) + "/Blue";
+                    case 3: document.getElementById(tempKey + "Button" + j).childNodes[3].innerHTML = "Core Count: " + formatNum(Cores, 0);
                         break
                 }
             }
@@ -113,7 +132,7 @@ function press(name, num) {
 }
 
 function increase(amnt) {
-    var next = amnt;
+    var next = amnt * IR;
     for (var i = 0; i < (player.unlock ? 3 : 2) ;i++){
         var temp = player.bars[Object.keys(player.bars)[i]];
         if(amnt >= 10000){
@@ -123,11 +142,11 @@ function increase(amnt) {
         }else{
         temp.width += next;
         next = 0;
-     while (temp.width > 100) {
-        temp.width -= 100;
+     while (temp.width > 256) {
+        temp.width -= 256;
         player.money[temp.name] += (temp.name == "blue" ? 1 : player.spectrumLevel[i+2]);
         if (temp.name == "blue") player.specreset += SG;
-        next += (temp.name == "red" ? IG : 5);
+        next += (temp.name == "red" ? IG : 8);
     }
   }
 }
@@ -156,35 +175,39 @@ function buyUpgrade(name, Bindex) {
 }
 
 function updateStats() {
-    RUM = (player.spectrumLevel[4] == 1 ? 1 + Math.log(Math.sqrt(player.money.red)) / Math.log(1e15) : 1);
-    PD = Math.pow(0.95, player.level.blue[1]);
-    RSM = 1 + player.level.blue[0] * (0.1 * player.spectrumLevel[0]);
-    IG = 5 * Math.pow(1.1, player.level.blue[2]);
-    RSS = Math.pow(0.95, player.level.blue[2]);
-    SG = player.level.blue[3] * 0.01;
-    click = Math.floor(((5 + player.level.red) * (((Math.floor(player.level.red / 10)) * 0.1) + 1)) * RSS);
-    auto = Math.pow(((((player.level.green * 10) * (((Math.floor(player.level.green / 10)) * 0.20) + 1)) * RSS) * RSM) * player.spectrumLevel[1], RUM);
+    RUM = (player.spectrumLevel[4] == 1 ? Math.max(1 + Math.log(Math.sqrt(player.money.red)) / Math.log(1e15),1) : 1);
+    PD = Math.pow(0.95, player.spectrumLevel[1]);
+    Clock = Math.pow(2, player.level.blue[0]);
+    IR = 8 + (8 * player.level.blue[1]);
+    IG = 8 + (8 * player.level.blue[2]);
+    Cores = Math.pow(2,player.level.blue[3]);
+    click = Math.floor(((8 + (player.level.red*1.5)) * ((Math.floor(player.level.red / 10)) * 0.1) + 1));
+    auto = Math.pow(((player.level.green * 16) * (((Math.floor(player.level.green / 10)) * 0.20) + 1)) * (Clock^Cores), RUM);
     price.red = 5 * Math.pow(1+((0.1 * Math.pow(1.25, Math.floor(player.level.red / 100))) * PD), player.level.red);
     price.green = 5 * Math.pow(1+((0.05 * Math.pow(1.25, Math.floor(player.level.green / 100))) * PD), player.level.green);
-    price.blue[0] = 5 * Math.pow(1.13, player.level.blue[0]);
-    price.blue[1] = 10 * Math.pow(1.22, player.level.blue[1]);
-    price.blue[2] = 25 * Math.pow(1.16, player.level.blue[2]);
-    price.blue[3] = 100 * Math.pow(10, player.level.blue[3]);
+    price.blue[0] = 1 * Math.pow(16, player.level.blue[0]);
+    price.blue[1] = 4 * Math.pow(4, player.level.blue[1]);
+    price.blue[2] = 8 * Math.pow(4, player.level.blue[2]);
+    price.blue[3] = 1048576 * Math.pow(64, player.level.blue[3]);
     SpecPrice[0] = Math.ceil(1 * Math.pow(1.5, player.spectrumLevel[0]-1));
     SpecPrice[1] = Math.ceil(3 * Math.pow(1.35, player.spectrumLevel[1]-1));
     SpecPrice[2] = Math.ceil(8 * Math.pow(1.75, player.spectrumLevel[2]-1));
     SpecPrice[3] = Math.ceil(15 * Math.pow(1.85, player.spectrumLevel[3]-1));
     if (player.bars.red.mouse == 1) income.red = (auto + (click*50)) / 100;
-    else income.red = (auto / 100);
-    income.green = (income.red*IG / 100);
-    income.blue = income.green*5 / 100;
+    else income.red = (auto / 256);
+    income.green = (income.red*IG / 256);
+    income.blue = income.green*8 / 256;
 }
 
-function formatNum(num, dp) {
+function formatNum(num, dp, type) {
     if (dp == undefined) dp = 2;
     var suffix = ["K", "M", "B", "T", "Qu", "Qi", "Sx", "Sp", "Oc", "No", "Dc"]
-    if (num < 10000) return num.toFixed(Math.min(Math.max(2 - Math.floor(Math.log10(num)), 0),dp));
-    else return (num / Math.pow(1000, Math.floor(Math.log(num) / Math.log(1000)))).toFixed(2 - Math.floor(Math.log10(num / Math.pow(1000, Math.floor(Math.log(num) / Math.log(1000)))))) + suffix[Math.floor(Math.log(num) / Math.log(1000)) - 1];
+    if (type == "Hz") {
+        suffix = ["nHz", "µHz", "mHz", "Hz", "kHz", "MHz", "GHz", "THz", "PHz", "EHz", "ZHz", "YHz"]
+        return (num / Math.pow(1000, Math.floor(Math.log(num) / Math.log(1024)))) + suffix[Math.floor(Math.log(num) / Math.log(1024))]
+    } else if (num < 10000) return num.toFixed(Math.min(Math.max(2 - Math.floor(Math.log10(num)), 0), dp));
+    else if (num < 1e36) return (num / Math.pow(1000, Math.floor(Math.log(num) / Math.log(1000)))).toFixed(2 - Math.floor(Math.log10(num / Math.pow(1000, Math.floor(Math.log(num) / Math.log(1000)))))) + suffix[Math.floor(Math.log(num) / Math.log(1000)) - 1];
+    else return num.toFixed(2);
 }
 
 function unlockBlue() {
@@ -221,7 +244,7 @@ function reset(type) {
             unlock: player.spectrumLevel[5] == 1,
             spectrum: Math.floor(player.specreset) + player.spectrum,
             specreset: 0,
-            spectrumLevel: player.spectrumLevel,
+            spectrumLevel: [player.spectrumLevel[0], player.spectrumLevel[1], player.spectrumLevel[2], player.spectrumLevel[3], player.spectrumLevel[4], player.spectrumLevel[5]],
             specced: player.specced +1,
         };
         player.bars = { red: new bar("red", 255, 0, 0, "redBar"), green: new bar("green", 0, 255, 0, "greenBar"), blue: new bar("blue", 0, 0, 255, "blueBar") };
@@ -262,3 +285,4 @@ function displayIncome(num) {
     if (num == income.green) num *= player.spectrumLevel[3];
     return(num)
 }
+
